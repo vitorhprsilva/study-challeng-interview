af6484d06f9de3e7bbf4c5b49361471e1da6f8e8
vitor <vitorhprsilva@gmail.com>
2023-06-26 18:36:18 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+interface Position {
+    positionX: number,
+    positionY: number
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [positions, setPosition] = useState<Position[]>([]);
+    const [positionsUndo, setPositionUndo] = useState<Position[]>([]);
+
+    const handleShowPoint = (event: React.MouseEvent<HTMLDivElement>): void => {
+        let positionAtual: Position = {positionX: event.clientX, positionY: event.clientY}
+        setPosition([...positions, positionAtual])
+    }
+
+    const Dot = ({positionY, positionX}: Position) => {
+        return <div className='dot' style={{position: "absolute",top: `${positionY}px`, left: `${positionX}px`}} />
+    }
+
+    const undoClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        event.stopPropagation()
+        const positionToUndo = positions?.at(positions.length-1)
+        if(positionToUndo) {
+            const {positionX, positionY} = positionToUndo
+            setPositionUndo([...positionsUndo, {positionX, positionY}]);
+            positions.pop()
+        }
+    }
+
+    const redoClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        event.stopPropagation()
+        const positionToRedo = positionsUndo?.at(positionsUndo.length-1)
+        if(positionToRedo) {
+            const {positionX, positionY} = positionToRedo
+            setPosition([...positions, {positionX, positionY}]);
+            positionsUndo.pop()
+        }
+    }
+
+    const Footer = () => {
+        return(
+            <div className="footer">
+                <button className="btn undo" onClick={(e) => undoClick(e)}>
+                    <img src="./assets/arrow-rotate-left.svg" alt="undo svg"/>
+                    undo
+                </button>
+                <button className="btn redo" onClick={(e) => redoClick(e)}>redo</button>
+            </div>
+        )
+    }
+
+      return (
+        <div className="page" onClick={(e) => handleShowPoint(e)} >
+                {
+                    positions.map((position, index) => {
+                        return(
+                            <Dot key={index} positionX={position.positionX} positionY={position.positionY} />
+                        )
+                    })
+                }
+            <Footer />
+        </div>
+      );
 }
 
 export default App;
